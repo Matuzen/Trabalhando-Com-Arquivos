527bb02f0428931dbec2c4ed7bf26967fa7d4968
Matheus Alexandre <matheuscurses@gmail.com>
2022-11-15 16:57:20 -0300

    File, FileInfo e IOException

---
+++ FileFileInfoIOException/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace FileFileInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"D:\file1.txt";    // caminho de origem
+            string targetPath = @"D:\file2.txt";    // caminho de destino
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);      // Ler todas as linhas do arquivo e guardar cada linha como elemento do vetor
+                foreach(string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
