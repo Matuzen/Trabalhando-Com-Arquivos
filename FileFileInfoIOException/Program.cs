efc8980b0045abd67f52ed75c00f5fbf843737a5
Matheus Alexandre <matheuscurses@gmail.com>
2022-11-17 22:50:06 -0300

    File Stram e StreamReader

--- FileFileInfoIOException/Program.cs
+++ FileFileInfoIOException/Program.cs
@@ -7,24 +7,32 @@ namespace FileFileInfo
     {
         static void Main(string[] args)
         {
-            string sourcePath = @"D:\file1.txt";    // caminho de origem
-            string targetPath = @"D:\file2.txt";    // caminho de destino
-
+            string path = @"D:\file1.txt";    // caminho de origem
+           
+            StreamReader sr = null;
             try
             {
-                FileInfo fileInfo = new FileInfo(sourcePath);
-                fileInfo.CopyTo(targetPath);
-                string[] lines = File.ReadAllLines(sourcePath);      // Ler todas as linhas do arquivo e guardar cada linha como elemento do vetor
-                foreach(string line in lines)
+
+                sr = File.OpenText(path);
+                while (!sr.EndOfStream)
                 {
+                    string line = sr.ReadLine();
                     Console.WriteLine(line);
                 }
+
             }
             catch (IOException e)
             {
                 Console.WriteLine("An error ocurred");
                 Console.WriteLine(e.Message);
             }
+
+            finally
+            {
+                if (sr != null) sr.Close();
+              
+            }
         }
     }
 }
+ 
